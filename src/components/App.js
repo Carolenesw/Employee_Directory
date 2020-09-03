a50b246c34c8c461cea15945489e0d2ef037b078
Carolene Wilson <carolenesw@gmail.com>
2020-09-03 15:18:42 -0400

    add App and Header components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./Header"
+// import logo from '/logo.svg';
+// import '/App.css';
+
+// create functional component div
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    </div>
+  );
+}
+
+export default App;
