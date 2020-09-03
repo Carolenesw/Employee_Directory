fe2ac5218539860cd36d08679251980f858e582f
Carolene Wilson <carolenesw@gmail.com>
2020-09-03 15:36:56 -0400

    create and style footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from "./Header"
+import Header from "./Header";
+import Footer from "./Footer";
 // import logo from '/logo.svg';
 // import '/App.css';
 
 // create functional component div
 function App() {
   return (
-    <div className="App">
+    <div>
     <Header />
+    <Footer />
     </div>
   );
 }
