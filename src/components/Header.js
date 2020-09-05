5619327ad0214371d541755b0b9bb6f4134f03a4
Carolene Wilson <carolenesw@gmail.com>
2020-09-04 21:04:11 -0400

    add search component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import Search from "./Search";
 
 function Header() {
 return <header>
     <h1 className="header">Employee Directory</h1>
+    <Search />
 </header>
+
 }
 
-export default Header
+
+export default Header;
