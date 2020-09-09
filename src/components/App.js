ef82207508cf56cdf858840ed88563dd4874a3ad
Carolene Wilson <carolenesw@gmail.com>
2020-09-09 15:06:25 -0400

    create class component to capture data for search function

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,43 @@
 // import all components for rendering
-import React from "react";
+import React, {Component} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "./Container";
+import DataReturn from "./DataReturn";
 // import logo from '/logo.svg';
 // import '/App.css';
 
-// create functional component div with components to be render
-function App() {
+// create class component to capture data for search function
+class App extends Component {
+
+state = {
+  search: ""
+}
+
+// get user input and handle change event
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+render (){
   return (
     <div>
-      <Header />
+      <Header 
+        handleInput = {this.handleInputChange}
+      />
+      {/* <p>{this.state.search}</p> */}
       <Container>
-          
+        <DataReturn />
       </Container>
       <Footer />
     </div>
   );
 }
+}
+  
 
 export default App;
