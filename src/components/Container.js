bf4d7f968796e6b9631414ea4a8aaa17464372a7
Carolene Wilson <carolenesw@gmail.com>
2020-09-10 15:14:53 -0400

    remove container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 function Container(props) {
-    return <container>
-        <div className={`container${props.fluid ? "-fluid" : ""}`}>{props.children}
+    return <div className={`container${props.fluid ? "-fluid" : ""}`}>{props.children}
         </div>
-    </container>
 }
 
 export default Container;
+
