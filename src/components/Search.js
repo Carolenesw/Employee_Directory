e2ab29bf81baf606301df93fea5d0392225911e2
Carolene Wilson <carolenesw@gmail.com>
2020-09-11 12:14:31 -0400

    create onclick function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function Search(props) {
   return (
     <form>
@@ -18,9 +19,16 @@ function Search(props) {
             placeholder="John Brown"
             id="search"
           />
-          <button className="btn my-2 my-sm-0" type="submit">
+          {/* <button onClick={props.handleInput} className="btn my-2 my-sm-0" type="submit">
             Search
-          </button>
+          </button> */}
+          <div className="input-group search">
+            <button
+              onClick={props.handleFormSubmit}
+              className="btn btn-danger btn-md red-search">
+              <i className="">Submit</i>
+            </button>
+          </div>
         </div>
       </div>
     </form>
